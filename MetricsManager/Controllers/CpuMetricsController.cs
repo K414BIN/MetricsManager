79e46823dabcc995bb5355fca8dbe601261c9376
Alexander Kalabin <k414bin@yandex.ru>
2021-03-30 22:30:44 +0300

    Logging CpuMetricsController successfuly

--- MetricsManager/Controllers/CpuMetricsController.cs
+++ MetricsManager/Controllers/CpuMetricsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MetricsManager.Controllers
 {
@@ -11,28 +12,20 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class CpuMetricsController : ControllerBase
     {
-        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
-        public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
-        {
-            return Ok();
-        }
+       
+            private readonly ILogger<CpuMetricsController> _logger;
 
-        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
-        public IActionResult GetMetricsByPercentileFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime,[FromRoute] Percentile percentile)
-        {
-            return Ok();
-        }
+            public CpuMetricsController(ILogger<CpuMetricsController> logger)
+            {
+                _logger = logger;
+                _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+            }
 
-        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
-        public IActionResult GetMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
-        {
-            return Ok();
+            [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+            public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+            {
+                _logger.LogInformation("Привет! Это наше первое сообщение в лог");
+                return Ok();
+            }
         }
-
-        [HttpGet("cluster/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
-        public IActionResult GetMetricsByPercentileFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime, [FromRoute] Percentile percentile)
-        {
-            return Ok();
-        }
-    }
 }
